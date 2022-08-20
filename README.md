10711a32eee82a4014f3871441b9d973406beb5d
jainsourabh2 <jainsourabh2@gmail.com>
2022-08-20 13:27:29 +0530

    load testing

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # logistics
 Demo on using BigTable and BigQuery
+git remote set-url origin https://jainsourabh2:<MYTOKEN>@github.com/jainsourabh2/logistics.git
+
+export GCP_PROJ_ID="on-prem-project-337210"
+export MAIN_PORT=8001
+export PORT_1=8002
+export PORT_2=8003
--- load-testing/locust.py
+++ load-testing/locust.py
@@ -3,12 +3,12 @@ from locust import HttpUser, task, between
 
 
 class WebsiteUser(HttpUser):
-    wait_time = between(1, 5)
-
-    # @task
-    # def index_page(self):
-    #     self.client.get(url="/hello")
+    #wait_time = between(1, 5)
 
     @task
     def slow_page(self):
-        self.client.get(url="/slow")
+        self.client.post(url="/api/orders/create",json=
+            {
+            "name":"sourabh"
+            }
+            )  
