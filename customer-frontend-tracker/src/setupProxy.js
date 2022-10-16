1e70cbcb73c4de7600a18c1f3404a5d42c956260
jainsourabh2 <jainsourabh2@gmail.com>
2022-10-17 00:21:58 +0530

    Updated URL

--- customer-frontend-tracker/src/setupProxy.js
+++ customer-frontend-tracker/src/setupProxy.js
@@ -7,8 +7,8 @@ module.exports = function (app) {
     app.use(
         '/api/',
         createProxyMiddleware({
-            target: 'https://bigtable-apis-q5cbfb3b6a-el.a.run.app/api/getAll'
-            //target: 'http://35.200.173.88:8080',
+            //target: 'https://bigtable-apis-q5cbfb3b6a-el.a.run.app'
+            target: 'http://35.200.173.88:8080',
             changeOrigin: true,
             secure: false,
             // pathRewrite: {
