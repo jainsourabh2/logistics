d9d2b70a43e66e1b668bd4158f50346b1c4c832e
jainsourabh2 <jainsourabh2@gmail.com>
2023-01-01 12:56:40 +0530

    Updated

--- dataflow-pipeline/dataflow_wrapper.sh
+++ dataflow-pipeline/dataflow_wrapper.sh
@@ -1,5 +1,6 @@
 cd ../dataflow-pipeline
 cp dataflow_generate_template_bkp.sh dataflow_generate_template.sh
+cp streaming-pubsub-to-bq-bigtable_bkp.py streaming-pubsub-to-bq-bigtable.py
 echo $1
 echo "Replacing <<project_id>> in dataflow_generate_template.sh file"
 sed -i "s/<<project_id>>/$1/g" "dataflow_generate_template.sh"
