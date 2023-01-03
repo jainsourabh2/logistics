c7ac7c6ab991fe705626227cb0399f87c6a5666b
jainsourabh2 <jainsourabh2@gmail.com>
2023-01-04 01:52:34 +0530

    Modified for modularization

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,11 +7,3 @@ module "gcp" {
   organization		= var.organization
   billing-account 	= var.billing-account
 }
-
-
-project			= "on-prem-project-337210"
-region			= "asia-south1"
-zone			= "asia-south1-a"
-folder			= "logistics-demo"
-organization	= "641067203402"
-billing-account	= "0144C2-0889E7-23B95D"
