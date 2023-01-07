4743046afa650f62d6279fd4d1746d986dd3628d
jainsourabh2 <jainsourabh2@gmail.com>
2023-01-07 16:05:53 +0530

    Updated for Documentation

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,9 @@
 module "gcp" {
-  source        	    = "./modules"
+  source          = "./modules"
+  region          = var.region
+  zone            = var.zone
+  organization    = var.organization
+  folder          = var.folder
+  project         = var.project
+  billing-account = var.billing-account
 }
