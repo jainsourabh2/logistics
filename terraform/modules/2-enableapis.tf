88f655a684f8b15b5dd492ffdcf6e890bc450838
Sourabh Jain <jainsourabh2@users.noreply.github.com>
2024-12-23 14:43:13 +0530

    Update 2-enableapis.tf

--- terraform/modules/2-enableapis.tf
+++ terraform/modules/2-enableapis.tf
@@ -1,3 +1,4 @@
+# Enable the APIs
 variable "services" {
   type = list(string)
   default = ["bigtable.googleapis.com","compute.googleapis.com","iam.googleapis.com","run.googleapis.com","cloudbilling.googleapis.com","containerregistry.googleapis.com","dns.googleapis.com","bigquery.googleapis.com","pubsub.googleapis.com","dataflow.googleapis.com","compute.googleapis.com","cloudbuild.googleapis.com"]
